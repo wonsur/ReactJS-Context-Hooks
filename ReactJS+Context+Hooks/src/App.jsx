0723c358083cb521d60e7bdc3e3522c7b02babc6
Szymon Wasowicz <wasowicz.szymon@gmail.com>
2024-11-20 23:32:43 +0100

    added footer and some small changes

--- ReactJS+Context+Hooks/src/App.jsx
+++ ReactJS+Context+Hooks/src/App.jsx
@@ -1,25 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function HeaderApp() {
   return (
     <header className="header-app">
-    <img className="header-app--logo" src="logo.png" alt="Logo"/>
-    My-shop aplication
+      <img className="header-app--logo" src="logo.png" alt="Logo" />
+      My-shop aplication
     </header>
   );
 }
 
-function Products(){
-
+function ProductList() {
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
-    fetch('http://private-1c19e-reactlesson.apiary-mock.com/products')
-    .then(response => response.json())
-    .then(data => setRecords( data))
-    .catch(err => console.log(err))
-  }, [])
-
+    fetch("http://private-1c19e-reactlesson.apiary-mock.com/products")
+      .then((response) => response.json())
+      .then((data) => setRecords(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   function AddToCart({ inStock }) {
     return inStock ? (
@@ -29,35 +27,56 @@ function Products(){
     );
   }
 
+  return (
+    <section className="product-list">
+      {/* tutaj ma byc search bar */}
 
-  return(
-      <>
-        {records.filter(x => x.id <= 5).map(x =>
-          <div className='product-box'>
-          <div><img className='product-box--image' src={x.photo}></img></div>
-          <div>
-            <h2>{x.name}</h2>
-            <pre>Cena: {x.price}zł</pre>
-            <p>{x.description}</p>
-            <AddToCart inStock={x.in_stock} />
+      {records
+        .filter((x) => x.id <= 5)
+        .map((x) => (
+          <div className="product-box">
+            <div>
+              <img className="product-box--image" src={x.photo}></img>
+            </div>
+            <div>
+              <h2>{x.name}</h2>
+              <pre>Cena: {x.price}zł</pre>
+              <p>{x.description}</p>
+              <AddToCart inStock={x.in_stock} />
+            </div>
           </div>
-          </div>
-        )}
-</>
-      
-
-  )
-
+        ))}
+    </section>
+  );
 }
 
-function App(){
-  return(
+function Footer() {
+  return <footer className="footer">Copyright by my-shop - 2018</footer>;
+}
+
+function Cart() {
+  return (
     <>
-    <HeaderApp />
-    <Products />
+      <section className="user-cart">
+        <h4>Twój koszyk</h4>
+        <ul className="user-cart--list"></ul>
+        <section className="total-price">Podsumowanie:</section>
+      </section>
     </>
-  )
+  );
 }
 
-  
+function App() {
+  return (
+    <>
+      <HeaderApp />
+      <div className="container">
+        <ProductList />
+        <Cart />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
 export default App;
