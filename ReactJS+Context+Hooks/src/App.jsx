1df03c383650079d5284ff33be23125d698756a5
Szymon Wasowicz <wasowicz.szymon@gmail.com>
2024-11-21 14:13:54 +0100

    added add to cart feature

--- ReactJS+Context+Hooks/src/App.jsx
+++ ReactJS+Context+Hooks/src/App.jsx
@@ -4,12 +4,12 @@ function HeaderApp() {
   return (
     <header className="header-app">
       <img className="header-app--logo" src="logo.png" alt="Logo" />
-      My-shop aplication
+      My-shop application
     </header>
   );
 }
 
-function ProductList() {
+function ProductList({ cart, setCart }) {
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
@@ -19,22 +19,35 @@ function ProductList() {
       .catch((err) => console.log(err));
   }, []);
 
-  function AddToCart({ inStock }) {
+  function handleAddToCart(product) {
+    setCart((prevCart) => [...prevCart, product]);
+  }
+
+  function AddToCart({ product, inStock }) {
     return inStock ? (
-      <button>Dodaj do koszyka</button>
+      <button onClick={() => handleAddToCart(product)}>Dodaj do koszyka</button>
     ) : (
       <button disabled>Dodaj do koszyka</button>
     );
   }
 
+  function Search() {
+    return (
+      <>
+        <div className="search">
+          <input type="text" className="search-input" placeholder="Szukaj..." />
+        </div>
+      </>
+    );
+  }
+
   return (
     <section className="product-list">
-      {/* tutaj ma byc search bar */}
-
+      <Search />
       {records
         .filter((x) => x.id <= 5)
-        .map((x) => (
-          <div className="product-box">
+        .map((x, index) => (
+          <div key={index} className="product-box">
             <div>
               <img className="product-box--image" src={x.photo}></img>
             </div>
@@ -42,7 +55,7 @@ function ProductList() {
               <h2>{x.name}</h2>
               <pre>Cena: {x.price}zł</pre>
               <p>{x.description}</p>
-              <AddToCart inStock={x.in_stock} />
+              <AddToCart product={x} inStock={x.in_stock} />
             </div>
           </div>
         ))}
@@ -50,29 +63,41 @@ function ProductList() {
   );
 }
 
-function Footer() {
-  return <footer className="footer">Copyright by my-shop - 2018</footer>;
-}
-
-function Cart() {
+function Cart({ cart }) {
+  const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
   return (
     <>
       <section className="user-cart">
         <h4>Twój koszyk</h4>
-        <ul className="user-cart--list"></ul>
-        <section className="total-price">Podsumowanie:</section>
+        {cart.length === 0 ? (
+          <p>Koszyk jest pusty</p>
+        ) : (
+          <ul className="user-cart--list">
+            {cart.map((item, index) => (
+              <li key={index}>
+                {index} - {item.name} - {item.price}zł
+              </li>
+            ))}
+          </ul>
+        )}
+        <section className="total-price">Podsumowanie: {totalPrice}zł</section>
       </section>
     </>
   );
 }
+function Footer() {
+  return <footer className="footer">Copyright by my-shop - 2018</footer>;
+}
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
     <>
       <HeaderApp />
       <div className="container">
-        <ProductList />
-        <Cart />
+        <ProductList cart={cart} setCart={setCart} />
+        <Cart cart={cart} />
       </div>
       <Footer />
     </>
